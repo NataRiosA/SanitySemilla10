e72644595bf4096bb023a6168984ccfce30afd6f
NataRiosA <nataliarios325@gmail.com>
2022-07-13 11:33:31 -0500

    Se ajustan botones de entrada de mercancia e inventario

--- src/test/java/com/indra/actions/InventoryAllocationActions.java
+++ src/test/java/com/indra/actions/InventoryAllocationActions.java
@@ -73,7 +73,7 @@ public class InventoryAllocationActions extends InventoryAllocationPage {
         getBtnCargaMasiva().click();
         Thread.sleep(1000);
         WebElement btnCargarArchivo = getDriver().findElement(By.id("formUpload:upload:file"));
-        btnCargarArchivo.sendKeys("C:\\SanitySemilla9\\src\\test\\resources\\config_data\\Inventario.csv");
+        btnCargarArchivo.sendKeys("C:\\Users\\nriosa\\Desktop\\SanitySemillas\\SanitySemilla10\\src\\test\\resources\\config_data\\Inventario.csv");
         Thread.sleep(3000);
         getBtnAccept().click();
         getBtnAcceptInventory().click();
